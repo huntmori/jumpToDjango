66b478ddc3034b1a142c1dd99e75bb09345afff9
huntmori <huntmori04x22@gmail.com>
2022-11-03 00:55:48 +0900

    2-06 데이터저장

--- pybo/views.py
+++ pybo/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question, Answer
+from django.utils import timezone
 
 
 def index(request):
@@ -17,3 +18,17 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    답변등록
+    :param request: content(string)
+    :param question_id: int
+    :return:
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    #question.answer_set.create(content=request.POST.get('content'), created_datetime=timezone.now())
+    answer = Answer(question=question, content=request.POST.get('content'), created_datetime=timezone.now())
+    answer.save()
+    return redirect('pybo:detail', question_id=question_id)
