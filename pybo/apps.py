195e6cbd007edac476170729391be95d620d2cab
huntmori <huntmori04x22@gmail.com>
2022-11-15 00:14:44 +0900

    2-09 템플릿 상속

--- pybo/apps.py
+++ pybo/apps.py
@@ -1,6 +1,8 @@
+import string
+
 from django.apps import AppConfig
 
 
 class PyboConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pybo'
+    name: string = 'pybo'
